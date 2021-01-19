d984e387aa63b318b5afecd4576fcd14dbc13ad5
Dennis Walangadi <dennisaw2000@gmail.com>
2021-01-20 00:00:23 +0700

    fix eslint

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  extends: ['eslint:recommended'],
+  rules: {
+    indent: ['error', 2],
+    'react/jsx-props-no-spreading': 'off',
+    'react/forbid-prop-types': 'off',
+    'react/jsx-uses-vars': 2,
+    'linebreak-style': 0,
+  },
+  parser: '@babel/eslint-parser',
+  env: {
+    browser: true,
+    node: true
+  },
+  plugins: [
+    'react',
+  ],
+  ignorePatterns: [
+    'node_modules/*'
+  ],
+};
